bdab0d008ef78c1cc34417c707602376b6b6a13e
Ellysha Chavez <ellyshachavez@gmail.com>
2019-04-06 11:09:16 -0700

    save my branch changes

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,5 @@ DROP DATABASE IF EXISTs verykoi;
 CREATE DATABASE verykoi;
 USE verykoi;
 
--- Create the table actors.
-CREATE TABLE users (
-  id int AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
-  email varchar(50) NOT NULL,
-  password varchar(60) NOT NULL,
-  PRIMARY KEY(id)
-);
+DROP DATABASE IF EXISTS post_db;
+CREATE DATABASE post_db;
