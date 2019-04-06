33dac132f46f4b3a108bf6c28e7c4d5222b0de4b
DobienC <dobien_c@yahoo.com>
2019-04-06 13:43:54 -0700

    Please Work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,30 +3,24 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(dbExamples) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
-        users: dbUsers
+        examples: dbExamples
       });
     });
-    // res.render("index");
   });
 
   // Load example page and pass in an example by id
-  app.get("/user/:id", function(req, res) {
-    db.User.findOne({ where: { id: req.params.id } }).then(function(
-      dbUser
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
     ) {
-      res.render("user", {
-        user: dbUser
+      res.render("example", {
+        example: dbExample
       });
     });
-    // res.render("example");
   });
-
-  // app.get("/tester", function(req, res) {
-  //   res.render(form);
-  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
