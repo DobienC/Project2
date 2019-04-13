f1edafc66e1f9306bce072a97fee8bf0dae4724f
Ellysha Chavez <ellyshachavez@gmail.com>
2019-04-12 21:53:17 -0700

    config

--- models/index.js
+++ models/index.js
@@ -4,14 +4,16 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
-var config = require(path.join(__dirname, "..", "config", "config.json"))[env];
+var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
+
 if (config.use_env_variable) {
     var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-var db = {};
+
 
 fs
     .readdirSync(__dirname)
